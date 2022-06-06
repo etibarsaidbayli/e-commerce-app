2726121a7b6edfebfba1bdaee3432cd7e6567a55
Etibarsaidbayli <etibarseidbekov@gmail.com>
2022-06-06 21:08:54 +0300

    add basket

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-const productsContainer = document.getElementById('productsContainer');
-const loadMoreButton = document.getElementById('loadMoreButton');
-const searchInput = document.getElementById('searchInput');
-
+const productsContainer = document.getElementById("productsContainer");
+const loadMoreButton = document.getElementById("loadMoreButton");
+const searchInput = document.getElementById("searchInput");
+const basketBtn = document.getElementById("basketBtn");
+let basketSpan = document.getElementById("basketSpan");
+let modalBasket = document.getElementById("modal-basket");
+const closerBasketModal = document.getElementById("closerBasketModal");
+const afterFlex = document.getElementById("afterFlex");
 const LIMIT = 8;
 let total = 0;
 let currentSkip = 0;
 
 fetchProductList(currentSkip);
 
-loadMoreButton.addEventListener('click', () => {
+loadMoreButton.addEventListener("click", () => {
   currentSkip += LIMIT;
   if (currentSkip >= total) {
     loadMoreButton.hidden = true;
@@ -17,36 +21,56 @@ loadMoreButton.addEventListener('click', () => {
   }
 });
 
-searchInput.addEventListener('input', () => {
+const debouncedSearch = _.debounce(handleSearch, 500);
+
+searchInput.addEventListener("input", debouncedSearch);
+
+function handleSearch() {
   currentSkip = 0;
   fetchProductList(currentSkip, searchInput.value);
-  productsContainer.innerHTML = '';
-});
+  productsContainer.innerHTML = "";
+}
+
+basketBtn.addEventListener("click", getShowBasket);
+closerBasketModal.addEventListener("click", getCloserBasket);
+
+function getShowBasket() {
+  modalBasket.classList.remove("translate-x-full");
+  modalBasket.classList.add("translate-x-0");
+}
+
+function getCloserBasket() {
+  modalBasket.classList.remove("translate-x-0");
+  modalBasket.classList.add("translate-x-full");
+}
 
 function disableButton() {
   loadMoreButton.disabled = true;
-  loadMoreButton.textContent = 'Loading...';
+  loadMoreButton.textContent = "Loading...";
 }
 
 function enableButton() {
   loadMoreButton.disabled = false;
-  loadMoreButton.textContent = 'Load more';
+  loadMoreButton.textContent = "Load more";
 }
 
-function fetchProductList(skip, q = '') {
+function fetchProductList(skip, q = "") {
   disableButton();
-  fetch(`https://dummyjson.com/products/search?limit=${LIMIT}&skip=${skip}&q=${q}`)
-    .then(res => res.json())
-    .then(data => {
+  fetch(
+    `https://dummyjson.com/products/search?limit=${LIMIT}&skip=${skip}&q=${q}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
       total = data.total;
-      let html = '';
+      let html = "";
       for (let product of data.products) {
         html += getProductHTML(product);
       }
-      productsContainer.insertAdjacentHTML('beforeend', html);
+      productsContainer.insertAdjacentHTML("beforeend", html);
     })
     .finally(() => {
       enableButton();
+      searchInput.value=""
     });
 }
 
@@ -60,10 +84,13 @@ function getProductHTML({ title, price, thumbnail }) {
         style="background-image: url('${thumbnail}');"
       >
         <button
-          class="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
+          data-title="${title}"
+          data-price="${price}"
+          data-thumbnail="${thumbnail}"
+          class="basket-plusBtn p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
         >
           <svg
-            class="h-5 w-5"
+            class="basket-plusSvg h-5 w-5"
             fill="none"
             stroke-linecap="round"
             stroke-linejoin="round"
@@ -83,4 +110,68 @@ function getProductHTML({ title, price, thumbnail }) {
       </div>
     </div>
   `;
+}
+
+productsContainer.addEventListener("click", addProductToBasket);
+
+function addProductToBasket(event) {
+  let downList = [];
+  if (event.target.matches(".basket-plusSvg")) {
+    if (event.target.tagName === "svg") {
+      const button = event.target.parentElement;
+      const { price, thumbnail, title } = button.dataset;
+
+      afterFlex.insertAdjacentHTML(
+        "afterend",
+        `<div class="flex justify-between mt-6">
+      <div class="flex">
+        <img
+          class="h-20 w-20 object-cover rounded"
+          src="${thumbnail}"
+          alt=""  
+        />
+        <div class="mx-3">
+          <h3 class="text-sm text-gray-600">${title}</h3>
+          <div class="flex items-center mt-2">
+            <button
+              class="text-gray-500 focus:outline-none focus:text-gray-600"
+            >
+              <svg
+                class="h-5 w-5"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
+            </button>
+            <span class="text-gray-700 mx-2">2</span>
+            <button
+              class="text-gray-500 focus:outline-none focus:text-gray-600"
+            >
+              <svg
+                class="h-5 w-5"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+      <span class="text-gray-600">${price}$</span>
+    </div>`
+      );
+    }
+  } 
 }
